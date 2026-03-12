411638bef1ccc68c72cdf5a807ffdb1ea2880f5d
iniLacher <adijpr25@gmail.com>
2026-03-12 23:48:06 +0700

    debug dan log

--- database.sql
+++ database.sql
@@ -1,3 +1,8 @@
 create database belajar_laravel_database; 
 
-use belajar_laravel_database;
+use belajar_laravel_database;create table categories (
+  id varchar (100) not null primary key,
+  name varchar(100) not null,
+  description text,
+  created_at datetime
+) engine InnoDB;
