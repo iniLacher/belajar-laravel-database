927ffeb61b7bc1384cbca06420f9f9767d4cc20a
iniLacher <adijpr25@gmail.com>
2026-03-26 20:57:07 +0700

    fixing bug inserting category,learn chunk, ordering , join with laravel

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -31,8 +31,8 @@ class QueryBuilderTest extends TestCase
   } 
   public function testInsert() {
     DB::table('categories')->insert([
-      'id' => 'GADGET',
-      'name' => 'Gadget',
+      'id' => 'LAPTOP',
+      'name' => 'Laptop',
     ]);
     DB::table('categories')->insert([
       'id' => 'FOOD',
@@ -45,10 +45,10 @@ class QueryBuilderTest extends TestCase
   }
 
   public function testSelect() {
-    $this->testInsert();
+    $this->insertCategories();
 
     $collection = DB::table('categories')->select('id', 'name')->get();
-    $this->assertCount(2, $collection);
+    $this->assertCount(5, $collection);
     
     $collection->each(function ($item) {
       Log::info(json_encode($item));
@@ -59,20 +59,20 @@ class QueryBuilderTest extends TestCase
     DB::table('categories')
     ->insert(['id' => 'FOOD', 'name' => 'Food', 'created_at' => '2022-01-01 00:00:00']);
     DB::table('categories')
-    ->insert(['id' => 'FASION', 'name' => 'Fasion', 'created_at' => '2022-01-01 00:00:00']);
+    ->insert(['id' => 'FASHION', 'name' => 'Fashion', 'created_at' => '2022-01-01 00:00:00']);
     DB::table('categories')
     ->insert(['id' => 'LAPTOP', 'name' => 'Laptop', 'created_at' => '2022-01-01 00:00:00']);
     DB::table('categories')
-    ->insert(['id' => 'KATOK', 'name' => 'Katok', 'created_at' => '2022-01-01 00:00:00']);
-    DB::table('categories')
     ->insert(['id' => 'MEJA', 'name' => 'Meja', 'created_at' => '2022-01-01 00:00:00']);
+    DB::table('categories')
+    ->insert(['id' => 'GADGET', 'name' => 'Gadget', 'created_at' => '2022-01-01 00:00:00']);
   }
 
   public function testWhere() {
     $this->insertCategories();
 
     $collection = DB::table('categories')->where(function(Builder $builder) {
-      $builder->where('id', '=', 'FASION');
+      $builder->where('id', '=', 'FASHION');
       $builder->orWhere('id', '=', 'LAPTOP');
     })->get();
     $this->assertCount(2, $collection);
@@ -84,7 +84,7 @@ class QueryBuilderTest extends TestCase
   public function testWhereBetween() {
     $this->testWhere();
 
-    $collection = DB::table('categories')->whereBetween('created_at', ['2022-01-01 00:00:00', '2022-01-03 00:00:00'])->get();
+    $collection = DB::table('categories')->whereBetween('created_at', ['2022-01-01 00:00:00', '2022-02-01 00:00:00'])->get();
     $this->assertCount(5, $collection);
     $collection->each(function ($item) {
       Log::info(json_encode($item));
@@ -94,7 +94,7 @@ class QueryBuilderTest extends TestCase
   public function testWhereIn() {
     $this->insertCategories();
 
-    $collection = DB::table('categories')->whereIn('id', ['FOOD', 'FASION'])->get();
+    $collection = DB::table('categories')->whereIn('id', ['FOOD', 'FASHION'])->get();
     $this->assertCount(2, $collection);
     $collection->each(function ($item) {
       Log::info(json_encode($item));
@@ -122,7 +122,7 @@ class QueryBuilderTest extends TestCase
   }
 
   public function testUpdate(){
-    $this->testInsert();
+    $this->insertCategories();
     DB::table('categories')->where('id', '=', 'FOOD')->update([
       'name' => 'Makanan'
     ]);
@@ -135,7 +135,7 @@ class QueryBuilderTest extends TestCase
   }
 
   public function testUpsert() {
-    $this->testInsert();
+    $this->insertCategories();
     DB::table('categories')->updateOrInsert([
       'id' => 'FOOD'
     ], [
@@ -150,11 +150,11 @@ class QueryBuilderTest extends TestCase
   }
 
   public function testDelete() {
-    $this->testInsert();
+    $this->insertCategories();
 
-    DB::table('categories')->where('id', '=', 'FOOD')->delete();
+    DB::table('categories')->where('id', '=', 'LAPTOP')->delete();
 
-    $collection = DB::table('categories')->where('id', '=', 'FOOD')->get();
+    $collection = DB::table('categories')->where('id', '=', 'LAPTOP')->get();
     $this->assertCount(0, $collection);
     $collection->each(function ($item) {
       Log::info(json_encode($item));  
@@ -162,11 +162,17 @@ class QueryBuilderTest extends TestCase
   }
 
   public function insertTableProduct() {
-    $this->testInsert();
+    $this->insertCategories();
     DB::table('product')
-    ->insert(['id' => '1', 'name' => 'Gadget','description' => 'gadget', 'created_at' => '2022-01-01 00:00:00', 'category_id' => 'GADGET']);
+    ->insert(['id' => '1', 'name' => 'Gadget','description' => 'Gadget', 'created_at' => '2022-01-01 00:00:00', 'category_id' => 'GADGET']);
     DB::table('product')
     ->insert(['id' => '2', 'name' => 'Food', 'description' => 'Food', 'created_at' => '2022-01-01 00:00:00', 'category_id' => 'FOOD']);
+    DB::table('product')
+    ->insert(['id' => '3', 'name' => 'Fashion', 'description' => 'Fashion', 'created_at' => '2022-01-01 00:00:00', 'category_id' => 'FASHION']);
+    DB::table('product')
+    ->insert(['id' => '4', 'name' => 'Meja', 'description' => 'Meja', 'created_at' => '2022-01-01 00:00:00', 'category_id' => 'MEJA']);
+    DB::table('product')
+    ->insert(['id' => '5', 'name' => 'Laptop', 'description' => 'Laptop', 'created_at' => '2022-01-01 00:00:00', 'category_id' => 'LAPTOP']);
   }
 
   public function testQueryBuilderJoin( ) {
@@ -175,9 +181,54 @@ class QueryBuilderTest extends TestCase
     $collection = DB::table('product')
     ->join('categories', 'product.category_id', '=', 'categories.id')
     ->select('product.id','product.name', 'categories.name as category_name')->get();
+    $this->assertCount(5, $collection);
+    $collection->each(function ($item) {
+      Log::info(json_encode($item));
+    });
+  }
+
+  public function testOrdering() {
+    $this->insertTableProduct();
+
+    $collection = DB::table('product')
+    ->whereNotNull('id')->orderBy('name', 'desc')->get();
+    $this->assertCount(5, $collection);
+    $collection->each(function ($item) {
+      Log::info(json_encode($item));
+    });
+  }
+
+  public function testPagination() {
+    $this->insertTableProduct();
+
+    $collection = DB::table('product')->skip(2)->take(2)->get();
     $this->assertCount(2, $collection);
     $collection->each(function ($item) {
       Log::info(json_encode($item));
     });
+  }
+
+  public function insertManyCategories() {
+    for ($i = 0; $i < 100; $i++) {
+      DB::table('categories')->insert([
+        'id' => 'CAT' . $i,
+        'name' => 'Category ' . $i,
+        'created_at' => '2022-01-01 00:00:00'
+      ]);
+    }
+  }
+
+  public function testChunk() {
+    $this->insertManyCategories();
+      Log::info('start chunk');
+
+    DB::table('categories')->chunkById(10, function ($categories) {
+      self::assertNotNull($categories);
+      $categories->each(function ($item) {
+        Log::info(json_encode($item));
+      });
+    });
+      Log::info('end chunk');
+
   }
 }
